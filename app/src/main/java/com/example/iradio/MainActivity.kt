1946b5446cef3adbf637be7d8592a8a8ffb0e091
ilnursoft <ilnursoft@gmail.com>
2024-08-28 14:33:56 +0300

    add mediaplayer не проигрывает но запускатеся на устройстве

--- app/src/main/java/com/example/iradio/MainActivity.kt
+++ app/src/main/java/com/example/iradio/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.iradio
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+import android.media.MediaPlayer
+
 class MainActivity : AppCompatActivity() {
+
+    private var mediaPlayer: MediaPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,49 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val statusRadio = findViewById<TextView>(R.id.statusRadio)
+        val buttonPlay: Button = findViewById(R.id.buttonPlay)
+        val buttonVolUp: Button = findViewById(R.id.buttonVolumeUp)
+        val buttonVolDown: Button = findViewById(R.id.buttonVolumeDown)
+        var statusPlay: Boolean = false // статус проигрывания текущей станции
+
+        buttonVolUp.setOnClickListener{
+            statusRadio.text = "VOLUME UP"
+        }
+
+        buttonVolDown.setOnClickListener{
+            statusRadio.text = "VOLUME DOWN"
+        }
+
+        buttonPlay.setOnClickListener{
+            if (statusPlay) {
+                statusRadio.text = "STOP PLAY RADIO"
+                buttonPlay.text = "Play"
+                statusPlay = false
+            }
+            else {
+                statusRadio.text = "PLAY RADIO"
+                buttonPlay.text = "Stop"
+                statusPlay = true
+
+                // Инициализация MediaPlayer
+                mediaPlayer = MediaPlayer().apply {
+                    setDataSource("http://cfm.jazzandclassic.ru:14536/rcstream.mp3") // URL на поток
+                    setOnPreparedListener {
+                        start() // Начало воспроизведения после подготовки
+                    }
+                    prepareAsync() // Асинхронная подготовка MediaPlayer
+                }
+            }
+
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // Освобождение ресурсов MediaPlayer при завершении активности
+        mediaPlayer?.release()
+        mediaPlayer = null
     }
 }
