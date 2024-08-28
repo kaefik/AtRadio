c0b2a6d8f61b59d482bfed993d8d6497f6f2bfeb
ilnursoft <ilnursoft@gmail.com>
2024-08-28 23:24:08 +0300

    fix макет приложения

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
+    implementation("com.intuit.sdp:sdp-android:1.1.0") // для построения адаптивного макета окна
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
