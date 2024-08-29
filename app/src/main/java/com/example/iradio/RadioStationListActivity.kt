13b2100d2a7b97b230cfcce46b39796a409cecfa
ilnursoft <ilnursoft@gmail.com>
2024-08-29 22:22:12 +0300

    fix

--- app/src/main/java/com/example/iradio/RadioStationListActivity.kt
+++ app/src/main/java/com/example/iradio/RadioStationListActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.app.Activity
 
 class RadioStationListActivity : AppCompatActivity() {
 
@@ -21,32 +23,18 @@ class RadioStationListActivity : AppCompatActivity() {
         val listView: ListView = findViewById(R.id.listViewRadioStations)
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, stationNames)
         listView.adapter = adapter
+
+        listView.setOnItemClickListener { _, _, position, _ ->
+            // Действие при выборе радиостанции из списка
+            val selectedStation = radioStations?.get(position)
+            if (selectedStation != null) {
+                val resultIntent = Intent()
+                resultIntent.putExtra("selectedStation", selectedStation)
+                setResult(Activity.RESULT_OK, resultIntent)
+                finish() // Закрытие активности после выбора станции
+            }
+        }
+
     }
 }
 
-
-//
-//import android.os.Bundle
-//import android.widget.ArrayAdapter
-//import android.widget.ListView
-//import androidx.appcompat.app.AppCompatActivity
-//
-//class RadioStationListActivity : AppCompatActivity() {
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_radio_station_list)
-//
-//        // Получаем список радиостанций из intent
-//        val radioStations = intent.getSerializableExtra("radioStations") as? ArrayList<RadioStation> ?: arrayListOf()
-//
-//        // Инициализируем ListView
-//        val listView: ListView = findViewById(R.id.listViewRadioStations)
-//
-//        // Создаем адаптер для отображения списка
-//        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, radioStations.map { it.name })
-//
-//        // Присваиваем адаптер ListView
-//        listView.adapter = adapter
-//    }
-//}
